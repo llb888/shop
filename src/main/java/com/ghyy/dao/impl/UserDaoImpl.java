b0a0cd82b7ce1ab5fa223b6a4e17300e60885287
aaa <4144@qq.com>
2020-07-16 21:22:19 +0800

    解决冲突

---
+++ src/main/java/com/ghyy/dao/impl/UserDaoImpl.java
@@ -0,0 +1,15 @@
+package com.ghyy.dao.impl;
+
+import com.ghyy.domain.User;
+
+public class UserDaoImpl {
+
+	public void add(User user) {
+
+	}
+
+	public User delete(User user) {
+
+		return user;
+	}
+}
